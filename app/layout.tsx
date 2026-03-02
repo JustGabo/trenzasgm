a11ecb1307908f41077c0209378ff1ce0bfb3fed
Gabriel <ovallegabriel123@gmail.com>
2026-03-02 13:21:16 -0400

    feat: enabling vercel analitycs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Montserrat } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const montserrat = Montserrat({
   variable: "--font-montserrat",
@@ -23,6 +24,7 @@ export default function RootLayout({
         className={`${montserrat.className} antialiased`}
       >
         {children}
+        <Analytics/>
       </body>
     </html>
   );
